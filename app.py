5ed11d0de0cc8d435d05cfd3ba08035de567bc46
mohamad solouki <mohamad.solouki@gmail.com>
2023-12-30 14:53:35 +0330

    update project

--- app.py
+++ app.py
@@ -1,39 +1,48 @@
-from flask import Flask, request, jsonify
+from PIL import Image
+from flask import Flask, request, jsonify, render_template
 import tensorflow as tf
 import numpy as np
 import cv2
 import os
-from werkzeug.utils import secure_filename
+from preprocess import preprocess_image
 
 app = Flask(__name__)
 model = tf.keras.models.load_model('emotion_detection_model.h5')
 
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
 @app.route('/predict', methods=['POST'])
-def predict():
-    if 'file' not in request.files:
+def predict_emotion():
+    file = request.files['file']
+    if not file:
         return jsonify({'error': 'No file provided'}), 400
 
-    file = request.files['file']
-    filename = secure_filename(file.filename)
-    file_path = os.path.join('uploads', filename)
-    file.save(file_path)
+    # Convert file stream to a numpy array
+    file_stream = file.stream.read()
+    npimg = np.frombuffer(file_stream, np.uint8)
+    image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
 
-    # Preprocessing the image
-    image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-    image = cv2.resize(image, (48, 48))
-    image = image / 255.0
-    image = np.expand_dims(image, axis=0)
-    image = np.expand_dims(image, axis=-1)
+    # Convert to grayscale as the model expects grayscale images
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    processed_image = preprocess_image(image)
 
-    # Predicting the emotion
-    prediction = model.predict(image)
+    # Predict the emotion
+    prediction = model.predict(processed_image)
     predicted_class = np.argmax(prediction, axis=1)
-
-    # Mapping the prediction to the actual emotion
     emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
     emotion = emotions[predicted_class[0]]
 
     return jsonify({'emotion': emotion})
 
+
+@app.route('/metrics', methods=['GET'])
+def get_metrics():
+    # Load metrics from a file or database
+    # Replace the below line with actual loading mechanism
+    metrics = {}  # Example: {"accuracy": 0.95, "f1_score": 0.94, ...}
+    return jsonify(metrics)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- preprocess.py
+++ preprocess.py
@@ -1,63 +1,27 @@
 import cv2
 import numpy as np
 
-def load_image(image_path):
+def preprocess_image(image):
     """
-    Load an image from a given path and convert it to grayscale.
+    Preprocess an image for the emotion detection model.
 
     Args:
-    image_path (str): Path to the image file.
-
-    Returns:
-    numpy.ndarray: Grayscale image.
-    """
-    # Read the image in grayscale mode
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    if image is None:
-        raise ValueError(f"Image not found at {image_path}")
-    return image
-
-def resize_image(image, size=(48, 48)):
-    """
-    Resize an image to a given size.
-
-    Args:
-    image (numpy.ndarray): Input image.
-    size (tuple): Desired size as (width, height).
-
-    Returns:
-    numpy.ndarray: Resized image.
-    """
-    resized_image = cv2.resize(image, size, interpolation=cv2.INTER_AREA)
-    return resized_image
-
-def normalize_image(image):
-    """
-    Normalize the pixel values of an image.
-
-    Args:
-    image (numpy.ndarray): Input image.
-
-    Returns:
-    numpy.ndarray: Normalized image.
-    """
-    # Normalize pixel values to the range [0,1]
-    normalized_image = image / 255.0
-    return normalized_image
-
-def preprocess_image(image_path):
-    """
-    Preprocess an image: load, convert to grayscale, resize, and normalize.
-
-    Args:
-    image_path (str): Path to the image file.
+    image (numpy.ndarray): Image array.
 
     Returns:
     numpy.ndarray: Preprocessed image ready for model input.
     """
-    image = load_image(image_path)
-    image = resize_image(image)
-    image = normalize_image(image)
-    # Reshape image to add batch dimension
+    # Ensure the image is in grayscale
+    if len(image.shape) == 3 and image.shape[2] == 3:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Resize image to 48x48
+    image = cv2.resize(image, (48, 48), interpolation=cv2.INTER_AREA)
+
+    # Normalize the image
+    image = image / 255.0
+
+    # Reshape image to add batch dimension for model input
     image = np.reshape(image, (1, *image.shape, 1))
+
     return image
